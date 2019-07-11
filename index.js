15cfc031e417baa1da6e9303fa3c4eac7eefae25
Tom White <tomjameswhite94@gmail.com>
2019-07-11 08:50:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ function takeANumber(deli, name) {
 }
 
 function takeANumber(deli) {
-  var i; i=1
-  deli.push()
+  var i; 
+  i=0; 
+  i++
+  deli.push(i)
+  return "Welcome, you have ticket number" + i
 }
 
 
