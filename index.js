d353978c97fc231ff40ca2ab81fe3a59a71b7b2f
Tom White <tomjameswhite94@gmail.com>
2019-07-11 07:45:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ function currentLine(deli) {
   if (deli.length === 0) {
     return "The line is currently empty."
   } else {
+    var text = "The line is currently: "
+    for (var i = 0; i<deli.length; i++) {
+      text+=i+1 + ". " + deli[i]
+      if (i + 1 < deli.length) {
+        text += ", "
+      }
+    }
+    return text
     
     return "The line is currently: 1. " + deli[0] +"," + " 2. " + deli[1] + "," + " 3. " + deli[2]
   }
